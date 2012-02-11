6d1ab27b0f99fecd5d37dd895aeb27adf74255f2
adamctemple <adam@brandingpersonality.com>
2012-02-11 12:26:53 -0800

    basic animations working

--- js/script.js
+++ js/script.js
@@ -4,14 +4,17 @@
 $(document).ready(function(){
 	$("#thumb1").click(function(event){
 		$("#gallery-window img").addClass('hidden');
+		$("#gallery-window img").removeAttr('style').animate({opacity: 'toggle'}, 500 );
 		$("#slide1").removeClass('hidden');
 	});
 	$("#thumb2").click(function(event){
 		$("#gallery-window img").addClass('hidden');
+		$("#gallery-window img").removeAttr('style').animate({opacity: 'toggle'}, 500 );
 		$("#slide2").removeClass('hidden');
 	});
 	$("#thumb3").click(function(event){
 		$("#gallery-window img").addClass('hidden');
+		$("#gallery-window img").removeAttr('style').animate({opacity: 'toggle'}, 500 );
 		$("#slide3").removeClass('hidden');
 	});
 });
