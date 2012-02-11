975a30fc7e888aac208626bd8e23d74fd4dcdac6
adamctemple <adam@brandingpersonality.com>
2012-02-11 11:46:01 -0800

    basic working demo with no animation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 /* Author:
-
+	Adam Temple
 */
-
+$(document).ready(function(){
+	$("#thumb1").click(function(event){
+		$("#gallery-window img").addClass('hidden');
+		$("#slide1").removeClass('hidden');
+	});
+	$("#thumb2").click(function(event){
+		$("#gallery-window img").addClass('hidden');
+		$("#slide2").removeClass('hidden');
+	});
+	$("#thumb3").click(function(event){
+		$("#gallery-window img").addClass('hidden');
+		$("#slide3").removeClass('hidden');
+	});
+});
 
 
 
